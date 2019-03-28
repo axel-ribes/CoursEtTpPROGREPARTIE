43c4578562f47decd2fdadc8aa03c90a63499979
RIBES Axel <r17004137@a-maq-iutinfo.salsa.univ-amu.fr>
2019-03-28 09:56:50 +0100

    ClientSmtp

--- TP/TP3Prog/src/MainClientSmtp.java
+++ TP/TP3Prog/src/MainClientSmtp.java
@@ -3,9 +3,9 @@ import java.io.IOException;
 public class MainClientSmtp {
     public static void main(String args[]) throws IOException {
 
-        ClientSmtp client = new ClientSmtp("EHLO Jupiter",25, "139.124.187.23");
+        ClientSmtp client = new ClientSmtp(,25, );
         for(int i=0;i<30;i++){
-            client.sendMail("ribes","gonand","Bonjour"+i, "hehehe");
+            client.sendMail(,,, );
         }
 
     }
